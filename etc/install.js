6f275e83ad69107a0b27daeee3dbf7aed8be3b6d
cburkey <cburkey@entermediasoftware.com>
2012-05-10 00:05:13 -0400

    Streamline

--- etc/install.js
+++ etc/install.js
@@ -3,8 +3,8 @@ importPackage( Packages.java.util );
 importPackage( Packages.java.lang );
 importPackage( Packages.com.openedit.modules.update );
 
-var appname = "app-emshare";
-var foldername = "emshare";
+var appname = "app-empush";
+var foldername = "empush";
 
 //http://dev.entermediasoftware.com/jenkins/job/app-emshare/lastSuccessfulBuild/artifact/deploy/app-emshare.zip
 var zip = "http://dev.entermediasoftware.com/jenkins/job/" + appname + "/lastSuccessfulBuild/artifact/deploy/" + appname + ".zip";
@@ -12,20 +12,19 @@ var zip = "http://dev.entermediasoftware.com/jenkins/job/" + appname + "/lastSuc
 var root = moduleManager.getBean("root").getAbsolutePath();
 var tmp = root + "/WEB-INF/tmp";
 
-log.add("1. GET THE LATEST ZIP FILE");
+log.add("1. GET THE LATEST ZIP FILE for " + appname);
 var downloader = new Downloader();
 downloader.download( zip, tmp + "/" + appname + ".zip");
 
+files.deleteAll( root + "/WEB-INF/base/" + foldername);
+
 log.add("2. UNZIP WAR FILE");
 var unziper = new ZipUtil();
-unziper.unzip(  tmp + "/" + appname + ".zip",  tmp );
+unziper.unzip(  tmp + "/" + appname + ".zip",  root );
 
+
+log.add("3. CLEAN UP");
 var files = new FileUtils();
-log.add("3. UPGRADE BASE DIR");
-files.deleteAll( root + "/WEB-INF/base" + foldername);
-files.copyFiles( tmp + "/" + foldername, root + "/WEB-INF/base/" + foldername + "/");
-
-log.add("4. CLEAN UP");
 files.deleteAll(tmp);
 
 log.add("5. UPGRADE COMPLETED");
